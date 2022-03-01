94c7592a4e9af36d7af6ab0a937dc4bc6a807e61
WillMenezesSH <wma_1999@hotmail.comcd Portugol/ clearcleargit config --global user.email wma_1999@hotmail.com>
2022-03-01 16:36:42 -0300

    Projeto Farmacia

---
+++ Farmacia/src/main/java/com/generation/categoria/model/Produto.java
@@ -0,0 +1,52 @@
+package com.generation.categoria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="Nome é obrigatório!")
+	private String nome;
+	
+	@NotBlank(message="Descrição é obrigatório!")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Produto getCategoria() {
+		return null;
+	}
+
+}
